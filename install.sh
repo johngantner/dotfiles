c106ffe95e70fed2c67096ce648ddd4359a937db
johngantner <127889384+johngantner@users.noreply.github.com>
2024-03-21 20:03:25 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,93 +1,81 @@
 #!/bin/bash
 
-# Check if effective user id is 0 (root)
-if [[ "$(id -u)" -eq 0 ]]; then
-    echo "Script is running as root"
+# get username that called script
+echo $SUDO_USER
+ME=$SUDO_USER
+
+# Check if running as root
+if [ "$(id -u)" -eq 0 ]; then
+    echo "Please run this script as a non-root user."
     exit 1
 fi
 
-# Check if apt is available
-if ! command -v apt &> /dev/null; then
-    echo "This script requires apt package manager, which is not available on your system."
+# Check for apt package manager
+if ! [ -x "$(command -v apt)" ]; then
+    echo "Error: apt package manager not found. This script is designed for Ubuntu Linux."
     exit 1
 fi
 
-# Define paths
-DOTFILES_DIR="$HOME/dotfiles"
-GITCONFIG_FILE="$HOME_DIR/gitconfig"
-BASHRC_FILE="$HOME_DIR/bashrc"
-AUTHORIZED_KEYS_FILE="$DOTFILES_DIR/authorized_keys"
-SSH_CONFIG_FILE="$DOTFILES_DIR/ssh_config"
+# Package installs
+sudo apt update
+sudo apt install -y dnsutils nmap
 
-# Install packages
-install_packages() {
-    sudo apt update
-    sudo apt install -y vim git
-}
+# Install Anaconda
+if [[ -e $(ls Anaconda3*.sh 2> /dev/null | head -1) ]]; then
+        echo "Installer found, running it"
+        bash Anaconda3-2024.02-1-Linux-x86_64.sh -b -p /home/$ME/anaconda3
+        echo "PATH=$PATH:/home/kduncan/anaconda3/bin" >> /home/$ME/.profile
 
-# Symbolically link files
-link_files() {
-    ln -sf "$GITCONFIG_FILE" "$HOME/.gitconfig"
-    ln -sf "$BASHRC_FILE" "$HOME/.bashrc"
-    ln -sf "$AUTHORIZED_KEYS_FILE" "$HOME/.ssh/authorized_keys"
-    ln -sf "$SSH_CONFIG_FILE" "$HOME/.ssh/config"
-}
+else
+        echo "Downloading anaconda installer"
+        curl -O https://repo.anaconda.com/archive/Anaconda3-2024.02-1-Linux-x86_64.sh                bash Anaconda3-2024.02-1-Linux-x86_64.sh -b -p /home/$ME/anaconda3
+        echo "PATH=$PATH:/home/kduncan/anaconda3/bin" >> /home/$ME/.profile
+fi
 
+# Command line games installation
+sudo apt install -y dnsutils nmap bastet
+
+# Git configuration
+git config --global user.name "John Gantner"
+git config --global user.name "" git config --global user.email "jgantner73@gmail.com"
+git config --global core.editor "vim"
+git config --global help.autocorrect 1
+
+# Symbolic link for .gitconfig
+ln -sf /home/jgantner/dotfiles/gitfiles ~/.gitconfig
+
+# Bash aliases
+echo 'alias ll="ls -al"' >> /home/$ME/.bashrc
+
+# Symbolic link for .bashrc
+ln -sf /home/jgantner/dotfiles ~/.bashrc
+
+# SSH public keys
+if [ ! -d ~/.ssh ]; then
+    mkdir ~/.ssh
+fi
+
+# Symbolic link for authorized_keys
+ln -sf /home/jgantner/authorized_keys ~/.ssh/authorized_keys
+
+# SSH config
+echo -e "Host fry.cs.wright.edu\n\tUser your_w_number\n\tIdentityFile ~/.ssh/id_rsa" > ~/.ssh/config
+
+# Vim customizations
 # Install Vundle
-install_vundle() {
-    if [ ! -d "$VUNDLE_DIR" ]; then
-        git clone https://github.com/VundleVim/Vundle.vim.git "$VUNDLE_DIR"
-    fi
-}
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
-# Install vim plugins
-install_vim_plugins() {
-    # Install Vundle plugins
-    vim +PluginInstall +qall
+# Install a color scheme
+mkdir -p ~/.vim/colors
+curl -o ~/.vim/colors/jellybeans.vim https://raw.githubusercontent.com/nanotech/jellybeans.vim/master/colors/jellybeans.vim
 
-    # Install color scheme
-    cp "$VIM_COLOR_SCHEME_FILE" "$HOME/.vim/colors/"
+# Install a Vim improvement plugin
+# Install NERDTree
+echo "Installing NERDTree..."
+vim -c "PluginInstall" -c "qa"
 
-    # Install Vim improvement plugin
-    vim +"PluginInstall your_vim_improvement_plugin" +qall
-}
+# Additional package installs for Vim plugins
+vim +PluginInstall +qall
+~/.vim/bundle/YouCompleteMe/install.py --clangd-completer
 
-# Change ownership of linked files to non-root user
-change_ownership() {
-    sudo chown -R $USER:$USER "$HOME/.gitconfig" "$HOME/.bashrc" "$HOME/.ssh/authorized_keys" "$HOME/.ssh/config" "$HOME/.vim" "$HOME/.vimrc"
-}
-
-# Create ~/.ssh directory if it doesn't exist
-create_ssh_directory() {
-    if [ ! -d "$HOME/.ssh" ]; then
-        mkdir "$HOME/.ssh"
-    fi
-}
-
-# Copy authorized_keys file to dotfiles directory
-copy_authorized_keys_to_repo() {
-    cp "$HOME/.ssh/authorized_keys" "$AUTHORIZED_KEYS_FILE"
-}
-
-# Create SSH config file with entry
-create_ssh_config_file() {
-    cat <<EOF > "$SSH_CONFIG_FILE"
-Host fry.cs.wright.edu
-    User your_username
-    # Add other configurations as needed
-EOF
-}
-
-# Main function
-main() {
-    install_packages
-    create_ssh_directory
-    copy_authorized_keys_to_repo
-    create_ssh_config_file
-    link_files
-    change_ownership
-    install_vim_plugins
-}
-
-# Run the main function
-main
+echo "Setup complete. Remember to restart your terminal for changes to take effect."
